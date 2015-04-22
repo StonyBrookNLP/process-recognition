4f5c3281c7bb4b9e84443c4b0cc670d537f2da44
chetannaik <chetannagarajnaik@gmail.com>
2015-04-22 19:45:35 -0400

    Auto-commit: saved entailment.py

--- entailment.py
+++ entailment.py
@@ -20,28 +20,28 @@ def get_list(sen):
 	return res
 
 def get_ai2_textual_entailment(t, h):
-    """
-    Returns the output of POST request to AI2 textual entailment service
+	"""
+	Returns the output of POST request to AI2 textual entailment service
 
-    Args:
-        t, h : text and hypothesis (two strings)
+	Args:
+		t, h : text and hypothesis (two strings)
 
-    Returns:
-        req : A text version of json response.
-        """
-        text = get_list(t)
-        hypothesis = get_list(h)
+	Returns:
+		req : A text version of json response.
+	"""
+	text = get_list(t)
+	hypothesis = get_list(h)
 
-    data = { "text" : text, "hypothesis": hypothesis}
+	data = { "text" : text, "hypothesis": hypothesis}
 
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
     # url = 'http://entailment.prod.allenai.org:8191/api/entails'
     url = 'http://localhost:8191/api/entails'
 
-    req = requests.post(url, headers=headers, data=json.dumps(data))
+	req = requests.post(url, headers=headers, data=json.dumps(data))
 
-    return req.json()
+	return req.json()
 
 def main():
 
